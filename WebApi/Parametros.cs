d66444bb4c6ca28b0843a1cc72358788071f9a5e
Eduardo Guerrero Reséndiz <a20021229@iteshu.edu.mx>
2026-05-16 14:21:40 -0600

    cambios

---
+++ WebApi/Parametros.cs
@@ -0,0 +1,36 @@
+﻿using DAL;
+
+namespace WebApi
+{
+    public static class Parametros
+    {
+        //escribir aqui la cadena de conexion 
+        /*
+         * @"Server=200.79.178.212; 
+                                                Port=3036; 
+                                                User=root;
+                                                Password=2d0Serv3r2025; 
+                                                Database=Delegacion; 
+                                                SslMode=None"
+         */
+#if DEBUG
+        public static string CadenaConexion = @"SERVER=CHAMITO\SQLEXPRESS; 
+                                                DATABASE=PrestaIteshu; 
+                                                Integrated Security=True;
+                                                TrustServerCertificate=True";
+#else
+        public static string CadenaConexion = @"Server=db52140.databaseasp.net; 
+                                                Database=db52140; 
+                                                User Id=db52140; 
+                                                Password=P-y2+g6Ec3H=; 
+                                                Encrypt=False; 
+                                                MultipleActiveResultSets=True; ";
+     
+#endif
+
+
+        public static TipoBD TipoBD = TipoBD.SQLServer;
+
+        public static FabricRepository FabricaRepository = new FabricRepository(CadenaConexion, TipoBD);
+    }
+}
